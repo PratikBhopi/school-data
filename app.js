555d74798d2b0345845d9e45efda65367f71e1d4
Pratik <pkbhopi132@gmail.com>
2025-03-11 01:09:29 +0530

    final

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ require('dotenv').config()
 const cors = require('cors')
 const app = express()
 const schoolRouter=require('./routes/school.routes')
+const { pool } = require('./db/db')
 app.use(cors())
 app.use(express.json())
 
 app.use("/school",schoolRouter)
 app.get('/',async (req,res)=>{
-    const [rows]=await db.query('SELECT * FROM schools')
+    const [rows]=await pool.query('SELECT * FROM schools')
     res.send('all schools: ',rows);
 })
 
